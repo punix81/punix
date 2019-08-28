26434b70fb3a0e6283d42bd81e21a1aee7967cb0
paulo lopes <paulo.lopes@bit.admin.ch>
2019-08-28 10:57:55 +0200

    add auth0 client id

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,7 +13,7 @@ export const environment = {
   // Configure Auth0 credentials
   auth0: {
     clientID: 'p752dE5xgLiPe5BvmTw8mDB1MPhCN4FS',
-    domain: 'dev-punix-eu.auth0.com'
+    domain: 'dev-punix.eu.auth0.com'
   }
 };
 
