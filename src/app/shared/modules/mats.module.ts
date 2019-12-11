84b8a2ca1f471f63e1ef4c13017e9dd037e8c2af
paulo lopes <paulo.lopes@bit.admin.ch>
2019-12-11 15:01:58 +0100

    add tslint config

--- src/app/shared/modules/mats.module.ts
+++ src/app/shared/modules/mats.module.ts
@@ -1,48 +1,49 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { 
-  MatButtonModule,
-  MatIconModule,
-  MatInputModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatStepperModule,
-  MatSelectModule,
-  MatDatepickerModule,
-  MatDialogModule,
-  MatNativeDateModule,
-  MatAutocompleteModule,
-  MatDividerModule,
-  MatSidenavModule,
-  MatListModule,
-  MatChipsModule,
-  MatMenuModule
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatStepperModule
 } from '@angular/material';
 
 const matModules = [
-  MatButtonModule,
-  MatIconModule,
-  MatInputModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatStepperModule,
-  MatSelectModule,
-  MatDatepickerModule,
-  MatDialogModule,
-  MatNativeDateModule,
-  MatAutocompleteModule,
-  MatDividerModule,
-  MatSidenavModule,
-  MatListModule,
-  MatChipsModule,
-  MatMenuModule
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatStepperModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatNativeDateModule,
+    MatAutocompleteModule,
+    MatDividerModule,
+    MatSidenavModule,
+    MatListModule,
+    MatChipsModule,
+    MatMenuModule
 ]
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [],
-  exports: matModules
+    imports: [
+        CommonModule
+    ],
+    declarations: [],
+    exports: matModules
 })
-export class MatsModule { }
+export class MatsModule {
+}
